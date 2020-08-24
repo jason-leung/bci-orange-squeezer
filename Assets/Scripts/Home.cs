1da833570904fe6cd97dee511502d9404dd5b6d1
jason-leung <leungj7049@gmail.com>
2020-08-24 19:37:29 -0400

    first commit

---
+++ Assets/Scripts/Home.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Home : MonoBehaviour
+{
+    GameObject howtoplay_panel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        howtoplay_panel = FindObjectOfType<Canvas>().transform.Find("HowToPlay_Panel").gameObject;
+    }
+
+    public void ExitApplication()
+    {
+        Application.Quit();
+    }
+
+    public void HowToPlay()
+    {
+        howtoplay_panel.SetActive(!howtoplay_panel.activeSelf);
+    }
+}
