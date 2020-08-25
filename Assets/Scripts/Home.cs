20c630d52af4f26f7bdba7ad82ad808c543fd050
jason-leung <leungj7049@gmail.com>
2020-08-25 02:57:33 -0400

    added water2d assets and game scene

--- Assets/Scripts/Home.cs
+++ Assets/Scripts/Home.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Home : MonoBehaviour
 {
@@ -14,6 +15,11 @@ public class Home : MonoBehaviour
     public void ExitApplication()
     {
         Application.Quit();
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
     }
 
     public void HowToPlay()
