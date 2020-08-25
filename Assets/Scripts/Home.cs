ca00eb6f804b3572f5d2c6c7acbb9f4c067db3d7
jason-leung <leungj7049@gmail.com>
2020-08-25 13:30:29 -0400

    added instructions page

--- Assets/Scripts/Home.cs
+++ Assets/Scripts/Home.cs
@@ -1,15 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Home : MonoBehaviour
 {
-    GameObject howtoplay_panel;
-    // Start is called before the first frame update
+    public GameObject howtoplay_panel;
+    public GameObject howtoplay_pageNumber;
+    public GameObject howtoplay_backButton;
+    public GameObject howtoplay_nextButton;
+    public int pageNumber;
+
     void Start()
     {
         howtoplay_panel = FindObjectOfType<Canvas>().transform.Find("HowToPlay_Panel").gameObject;
+        howtoplay_pageNumber = howtoplay_panel.transform.Find("PageNumber").gameObject;
+        howtoplay_backButton = howtoplay_panel.transform.Find("Back_Button").gameObject;
+        howtoplay_nextButton = howtoplay_panel.transform.Find("Next_Button").gameObject;
+        pageNumber = System.Int32.Parse(howtoplay_pageNumber.GetComponent<Text>().text);
+        howtoplay_backButton.SetActive(false);
     }
 
     public void ExitApplication()
@@ -24,6 +35,34 @@ public class Home : MonoBehaviour
 
     public void HowToPlay()
     {
+        howtoplay_pageNumber.GetComponent<Text>().text = "1";
+        howtoplay_backButton.SetActive(false);
+        howtoplay_nextButton.SetActive(true);
+        for (int i = 1; i <= 9; i++) howtoplay_panel.transform.Find("Page" + i).gameObject.SetActive(i == 1);
         howtoplay_panel.SetActive(!howtoplay_panel.activeSelf);
+    }
+
+    public void HowToPlayLastPage()
+    {
+        pageNumber = System.Int32.Parse(howtoplay_pageNumber.GetComponent<Text>().text);
+        pageNumber -= 1;
+        pageNumber = Math.Max(1, pageNumber);
+        howtoplay_pageNumber.GetComponent<Text>().text = pageNumber.ToString();
+        howtoplay_backButton.SetActive(pageNumber != 1);
+        howtoplay_nextButton.SetActive(pageNumber != 9);
+
+        for (int i = 1; i <= 9; i++) howtoplay_panel.transform.Find("Page" + i).gameObject.SetActive(i == pageNumber);
+    }
+
+    public void HowToPlayNextPage()
+    {
+        pageNumber = System.Int32.Parse(howtoplay_pageNumber.GetComponent<Text>().text);
+        pageNumber += 1;
+        pageNumber = Math.Min(9, pageNumber);
+        howtoplay_pageNumber.GetComponent<Text>().text = pageNumber.ToString();
+        howtoplay_backButton.SetActive(pageNumber != 1);
+        howtoplay_nextButton.SetActive(pageNumber != 9);
+
+        for (int i = 1; i <= 9; i++) howtoplay_panel.transform.Find("Page" + i).gameObject.SetActive(i == pageNumber);
     }
 }
